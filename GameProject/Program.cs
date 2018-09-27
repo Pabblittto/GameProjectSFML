73d31a587b4e31d70c83b0a40b1b71a9ff0abedc
Pabblo <pawel.mat17@op.pl>
2018-09-27 13:45:07 +0200

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow Window = new RenderWindow(new VideoMode(1800, 800), "Game Tests!!");
+
+
+
+            while(Window.IsOpen)
+            {
+                Window.Clear();
+                Window.DispatchEvents();
+
+
+
+                Window.Display();
+            }
+
+
+
+
+        }
+    }
+}
